bbb25895c256905d01d86ab5f2745aaf8e7cfad7
Andrei <andreiconova@gmail.com>
2023-10-28 22:27:35 +0300

    No Errors

--- standaloneClient/src/module-info.java
+++ standaloneClient/src/module-info.java
@@ -5,5 +5,4 @@
  * 
  */
 module standaloneClient {
-	requires java.naming;
 }
--- standaloneClient/src/standaloneClient/Main.java
+++ standaloneClient/src/standaloneClient/Main.java
@@ -3,10 +3,16 @@ package standaloneClient;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
 
+import com.example.FirstStatelessEjbRemote;
 
 public class Main {
+
 	public static void main(String[] args) throws NamingException {
+		// TODO Auto-generated method stub
 		InitialContext context  = new InitialContext();
-		
+		FirstStatelessEjbRemote firstEjb=(FirstStatelessEjbRemote) context
+				.lookup("java:global/myFirstEar/myFirstEjb/FirstStatelessEjb!com.example.FirstStatelessEjb");
+		firstEjb.insert("Andrei Conoval");
 	}
+
 }
